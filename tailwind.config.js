0063520a8bd1715cbd74cd923e380d8a814ff7a4
unknown <103291417+MaxRodrigurs@users.noreply.github.com>
2023-11-24 14:40:14 -0300

    starting project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      'phone': '280px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'desktop': '1280px',
+      'up': '1430px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
